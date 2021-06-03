26a4bb849dc0edffea079fb2e0e237c28396fe72
wemali05 <alfredwemali05@gmail.com>
2021-06-03 06:52:40 +0300

    use trix editor

--- resources/views/discussions/create.blade.php
+++ resources/views/discussions/create.blade.php
@@ -10,7 +10,8 @@
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <textarea type="text" rows="3" class="form-control" id="content" name="content"></textarea>
+                     <input id="content" type="hidden" name="content">
+                     <trix-editor input="content"></trix-editor>
                 </div>
                  <div class="form-group">
                     <label for="channel">Channel</label>
@@ -24,4 +25,12 @@
             </form>
         </div>    
     </div>
+@endsection
+
+@section('css')
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.css" integrity="sha512-5m1IeUDKtuFGvfgz32VVD0Jd/ySGX7xdLxhqemTmThxHdgqlgPdupWoSN8ThtUSLpAGBvA8DY2oO7jJCrGdxoA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
+@section('js')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js" integrity="sha512-2RLMQRNr+D47nbLnsbEqtEmgKy67OSCpWJjJM394czt99xj3jJJJBQ43K7lJpfYAYtvekeyzqfZTx2mqoDh7vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>">
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +17,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @yield('css')
 </head>
 <body>
     <div id="app">
@@ -99,7 +100,10 @@
                 @yield('content')
             </main>
         @endauth
-        
     </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('js')
 </body>
 </html>
