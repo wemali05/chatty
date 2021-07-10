a59408d2755fe93600145049fc3cff9431a102b9
wemali05 <alfredwemali05@gmail.com>
2021-07-10 13:25:36 +0300

    gravator fix

--- resources/views/partials/discussion-header.blade.php
+++ resources/views/partials/discussion-header.blade.php
@@ -1,7 +1,7 @@
  <div class="card-header">
     <div class="d-flex justify-content-between">
         <div>
-            <img  src="{{Auth::user()->gravatar}}" width="40" height="40" style="border-radius: 50%" alt="Img">
+            <img  src="{{$discussion->author->gravatar}}" width="40" height="40" style="border-radius: 50%" alt="Img">
             <strong>{{ $discussion->author->name}}</strong>
         </div>
         <div>
