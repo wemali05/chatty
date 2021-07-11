4b8bf1eb3c1db4c4f268473b17fa8cc2287cf5a5
wemali05 <alfredwemali05@gmail.com>
2021-07-11 18:26:20 +0300

    use queues job on notification

--- app/Notifications/ReplyMarkedAsBestReply.php
+++ app/Notifications/ReplyMarkedAsBestReply.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ReplyMarkedAsBestReply extends Notification
+class ReplyMarkedAsBestReply extends Notification implements ShouldQueue
 {
     use Queueable;
 
