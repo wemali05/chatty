cf5dcf7e857875b8fe00ee43cbad920f56426e0b
wemali05 <alfredwemali05@gmail.com>
2021-07-12 18:37:55 +0300

    remove bold font classes

--- resources/views/users/notifications.blade.php
+++ resources/views/users/notifications.blade.php
@@ -12,12 +12,12 @@
                     <li class="list-group-item d-flex align-items-center justify-content-between">
                         @if( $notification->type === 'App\Notifications\NewReplyAdded' )
                          A new reply was Aded:
-                        <strong class="font-weight-bold">{{ $notification->data['discussion']['title'] }}</strong>
+                        <strong>{{ $notification->data['discussion']['title'] }}</strong>
                         <a href="{{ route('discussions.show', $notification->data['discussion']['slug'] )}}" class="btn btn-info text-white float-right">view Discusion</a>
                         @endif
                         @if( $notification->type === 'App\Notifications\ReplyMarkedAsBestReply')
                         Your reply was marked as Best Reply:
-                        <strong class="font-weight-bold">{{ $notification->data['discussion']['title'] }}</strong>
+                        <strong>{{ $notification->data['discussion']['title'] }}</strong>
                         <a href="{{ route('discussions.show', $notification->data['discussion']['slug'] )}}" class="btn btn-info text-white float-right">view Discusion</a>
                         @endif
                     </li>
